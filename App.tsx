2b0466899b5696fc2a8e4d9006eea4ce608da144
Andreas Wilson <andreasw@gmail.com>
2020-11-16 12:35:06 +0100

    Added movie card

--- App.tsx
+++ App.tsx
@@ -2,12 +2,30 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { Navigator } from './client/navigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { ThunkAction } from 'redux-thunk';
+import movieSliceReducer, {
+  MovieState,
+} from './client/features/movies/MovieSlice';
+import { configureStore, Action } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+// The AppThunk type is used for type definitions for thunk actions
+export type AppThunk = ThunkAction<void, MovieState, unknown, Action<string>>;
+
+// Configure redux store
+const store = configureStore({
+  reducer: {
+    movieStore: movieSliceReducer,
+  },
+});
 
 const App: React.FunctionComponent = (): JSX.Element => {
   return (
-    <NavigationContainer>
-      <Navigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Navigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
--- client/navigator/index.tsx
+++ client/navigator/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from '../../screens/HomeScreen';
+import { HomeScreen } from '../screens/HomeScreen';
+import { MovieParams, MovieScreen } from '../screens/MovieScreen';
+import { useSelector } from 'react-redux';
+import { movieSelector } from '../features/movies/MovieSlice';
+
 export type StackParamList = {
   // Define which parameters can be passed to each of the screens of the navigator
   Home: undefined;
+  Movie: MovieParams;
+  // Movie: undefined;
 };
+
+export enum AppScreens {
+  Home = 'Home',
+  Movie = 'Movie',
+}
 
 const Stack = createStackNavigator<StackParamList>();
 
 export const Navigator: React.FunctionComponent = () => {
+  const { header } = useSelector(movieSelector);
   return (
     //   Hide the default header with the back button
     <Stack.Navigator>
@@ -16,6 +28,11 @@ export const Navigator: React.FunctionComponent = () => {
         name={'Home'}
         component={HomeScreen}
         options={{ title: 'NTNU Movie DB' }}
+      />
+      <Stack.Screen
+        name={AppScreens.Movie}
+        component={MovieScreen}
+        options={{ title: header }}
       />
     </Stack.Navigator>
   );
